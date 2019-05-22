a0a05e5bd5210715a3f27d8858bdf762cd031bcb
Syeda <syed.ali.shah3938@gmail.com>
2019-05-22 23:44:20 +0500

    Setting Activity

--- app/src/main/java/com/tool/kustiit/myshop/SettingsActivity.java
+++ app/src/main/java/com/tool/kustiit/myshop/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.tool.kustiit.myshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
     }
 }
