56bf23b6108b34eaac21c167e2a634ec9223c204
ali-shahkust <syed.ali.shah3938@gmail.com>
2019-06-15 15:38:19 +0500

    Update in Cart

--- app/src/main/java/com/tool/kustiit/myshop/ProductDetailActivity.java
+++ app/src/main/java/com/tool/kustiit/myshop/ProductDetailActivity.java
@@ -4,12 +4,18 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,6 +23,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 import com.tool.kustiit.myshop.Model.Products;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
@@ -26,6 +36,8 @@ public class ProductDetailActivity extends AppCompatActivity {
     private ImageView productImgDetail ;
     private TextView price , description , name ;
     private FirebaseAuth mAuth;
+    private Button addtoCartbtn;
+
 
     private  String ProductID = "";
 
@@ -39,6 +51,10 @@ public class ProductDetailActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
+
+        mAuth = FirebaseAuth.getInstance();
+
+        addtoCartbtn = (Button)findViewById(R.id.addToCart);
 
 
 
@@ -55,6 +71,66 @@ public class ProductDetailActivity extends AppCompatActivity {
 
 
         getProductDetails(ProductID);
+
+        addtoCartbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddToCartList();
+            }
+        });
+
+    }
+
+    private void AddToCartList() {
+
+        String SaveCurrentTime ;
+        String SaveCurrentDate;
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        SaveCurrentDate = currentDate.format(calendar.getTime());
+        SimpleDateFormat currentTime = new SimpleDateFormat("MMM dd, yyyy");
+        SaveCurrentTime = currentTime.format(calendar.getTime());
+
+
+     final    DatabaseReference cartItems = FirebaseDatabase.getInstance().getReference().child("CartList");
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        String userId = null;
+        if (currentUser != null) {
+            userId = currentUser.getUid();
+        }
+
+        final HashMap<String, Object> add_data = new HashMap<>();
+
+        add_data.put("pid", ProductID);
+        add_data.put("pName", name.getText().toString());
+        add_data.put("price", price.getText().toString());
+        add_data.put("quantity", elegantNumberButton.getNumber());
+        add_data.put("date", SaveCurrentDate);
+        add_data.put("time", SaveCurrentTime);
+        add_data.put("discount", "");
+
+        final String finalUserId = userId;
+        cartItems.child("User_view").child(userId).child("Products")
+                .child(ProductID).updateChildren(add_data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    cartItems.child("Admin_view").child(finalUserId).child("Products")
+                            .child(ProductID).updateChildren(add_data).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(ProductDetailActivity.this, "Added To Cart", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
 
     }
 
