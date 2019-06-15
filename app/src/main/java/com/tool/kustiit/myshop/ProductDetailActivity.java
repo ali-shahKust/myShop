d66ec34ba7ca8049617983e7bd2567f91d300e1d
ali-shahkust <syed.ali.shah3938@gmail.com>
2019-06-15 15:50:40 +0500

    Update in Cart

--- app/src/main/java/com/tool/kustiit/myshop/ProductDetailActivity.java
+++ app/src/main/java/com/tool/kustiit/myshop/ProductDetailActivity.java
@@ -1,5 +1,6 @@
 package com.tool.kustiit.myshop;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -124,6 +125,9 @@ public class ProductDetailActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<Void> task) {
                             if(task.isSuccessful()){
                                 Toast.makeText(ProductDetailActivity.this, "Added To Cart", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent (ProductDetailActivity.this , HomeActivity.class);
+                                startActivity(intent);
                             }
                         }
                     });
